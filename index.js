807dc8a6af6bdb66877c6f74575cf568972725e0
MS <miro.sorja@gmail.com>
2016-11-06 18:04:12 +0200

    Fixed some minor bugs, ex. module.exports, and assign instead of spread

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-exports.requireAuthentication = function(store, routeObject, statePath = 'auth', redir = '') {
-  return {
-    ...routeObject,
-    onEnter: (nextState, replace, callback) => {
-      const state = store.getState()
-      const auth = statePath.split('.').reduce((o, i) => o[i], state)
-      if (!auth) {
-        console.warn('Not authenticated. Redirecting')
-        replace(`/${redir}`)
+"use strict";
+
+module.exports = function(store, routeObject, statePath = 'auth', redir = '') {
+  return Object.assign(
+    routeObject,
+    {
+      onEnter: (nextState, replace, callback) => {
+        const state = store.getState()
+        const auth = statePath.split('.').reduce((o, i) => o[i], state)
+        if (!auth) {
+          console.warn('Not authenticated. Redirecting')
+          replace(`/${redir}`)
+        }
+        callback()
       }
-      callback()
-    }
-  }
+    })
 }
